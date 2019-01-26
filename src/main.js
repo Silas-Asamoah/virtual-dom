8f0a213b77cbe84a48f7edc22bcd5e2bed29663a
Silas Bempong <silasbempong@gmail.com>
2019-01-26 06:54:15 +0000

    :file_folder:

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-const vApp = {
+import createElement from './vdom/createElement';
+
+
+const vApp = createElement('div', {
+    attrs: {
+        id: 'app',
+    }
+});
+
+/*const vApp = {
     tagName: 'div',
     attrs: {
         id: 'app',
     },
 };
-
+*/
 console.log(vApp);
+
+
+
+
