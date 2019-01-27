31b6d02f6157b905b68e37bfab6b574ff1b048d2
Silas Bempong <silasbempong@gmail.com>
2019-01-27 05:33:00 +0000

    :fireworks: addition of textnode render

--- src/vdom/render.js
+++ src/vdom/render.js
@@ -1,12 +1,12 @@
-const render = (vNode) => {
+const renderElem = ({tagName, attrs, children}) => {
 
     //Element creation
-    const $el = document.createElement(vNode.tagName);
+    const $el = document.createElement(tagName);
 
     //Adding all the attributes attributed to vNode.attrs
     //eg. <div id="app"></div>
 
-    for (const [k, v] of Object.entries(vNode.attrs)){
+    for (const [k, v] of Object.entries(attrs)){
         $el.setAttribute(k, v);
     }
 
@@ -20,5 +20,15 @@ const render = (vNode) => {
     return $el;
 
 };
+
+
+const render = (vNode) => {
+    if (typeof vNode === 'string'){
+        return document.createTextNode(vNode);
+    }
+
+    return renderElem(vNode);
+};
+
 
 export default render;
