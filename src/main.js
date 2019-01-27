f5c149f2da934e65b0d0b7fe4714470c22ef3c34
Silas Bempong <silasbempong@gmail.com>
2019-01-27 07:06:34 +0000

    count increment

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import createElement from './vdom/createElement';
 import render from './vdom/render';
+import mount from './vdom/mount';
 
 
-const vApp = createElement('div', {
+const createVApp = count => createElement('div', {
     attrs: {
         id: 'app',
+        dataCount: count,
     },
     children: [
-        'Hello world', //represents TextNode
+        'The current count is: ', //represents TextNode
+        String(count),
         createElement('img', {
             attrs: {
                 src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif',
@@ -23,9 +26,21 @@ const vApp = createElement('div', {
     },
 };
 */
-const $app = render(vApp);
 
-console.log($app);
+let count = 0;
+const vApp = createVApp(count);
+const $app = render(vApp);
+let $rootEl = mount($app, document.getElementById('app'));
+
+setInterval(() => {
+    count++;
+    $rootEl = mount(render(createVApp(count)), $rootEl);
+}, 1000);
+
+//console.log($app);
+
+//Mount $app to the empty div
+//mount($app, document.getElementById('app'));
 
 
 
