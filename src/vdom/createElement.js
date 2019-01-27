f79563bcbcacc7b00aa57637f5fc2294dee4870c
Silas Bempong <silasbempong@gmail.com>
2019-01-27 04:47:19 +0000

    :shower: DOM purification by using Object.create(null)

--- src/vdom/createElement.js
+++ src/vdom/createElement.js
@@ -1,7 +1,24 @@
-export default (tagName, { attrs = {}, children = [] } = {}) => {
+/*export default (tagName, { attrs = {}, children = [] } = {}) => {
     return{
         tagName,
         attrs,
         children,
     };
+};
+
+*/
+
+//Making the DOM purer using the Object.create(null)
+//This creates a truly plain object that doesn't inherit from Object but null instead
+
+export default (tagName, { attrs = {}, children = [] } = {}) => {
+    const vElem = Object.create(null);
+
+    Object.assign(vElem, {
+        tagName,
+        attrs,
+        children,
+    });
+    
+    return vElem;
 };
